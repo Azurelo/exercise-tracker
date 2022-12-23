20b99b4dbea59b6f048375ce62c416a9ae3def64
Justus <galaxycheesemc@gmail.com>
2022-12-22 19:50:35 -0700

    Added deselecting

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,7 +46,7 @@ $.ajax({
   let selectedOption3;
 
   // Add change event listeners to the dropdown elements
- /*  dropdown1.addEventListener('change', function() {
+  dropdown1.addEventListener('change', function() {
     selectedOption1 = this.value;
   });
   dropdown2.addEventListener('change', function() {
@@ -54,42 +54,53 @@ $.ajax({
   });
   dropdown3.addEventListener('change', function() {
     selectedOption3 = this.value;
-  }); */
+  });
 
     // Get the card and the dropzones
-    var card = document.getElementById("card");
-    var dropzones = document.querySelectorAll(".dropzone");
-  
-    // Add the "draggable" attribute to the card
-    card.setAttribute("draggable", "true");
-  
-    // Add the dragstart event listener to the card
-    card.addEventListener("dragstart", function(event) {
-      // Set the data type and the value of the drag data
-      event.dataTransfer.setData("text/plain", event.target.id);
+  // Get the card, the dropzones, and the original div
+  var card = document.getElementById("card");
+  var dropzones = document.querySelectorAll(".dropzone");
+  var originalDiv = document.getElementById("original-div");
+
+  // Add the "draggable" attribute to the card
+  card.setAttribute("draggable", "true");
+
+  // Add the dragstart event listener to the card
+  card.addEventListener("dragstart", function(event) {
+    // Set the data type and the value of the drag data
+    event.dataTransfer.setData("text/plain", event.target.id);
+  });
+
+  // Add the dragover event listener to the dropzones
+  dropzones.forEach(function(dropzone) {
+    dropzone.addEventListener("dragover", function(event) {
+      // Prevent the default behavior (prevent the card from being dropped outside of the dropzone)
+      event.preventDefault();
     });
-  
-    // Add the dragover event listener to the dropzones
-    dropzones.forEach(function(dropzone) {
-      dropzone.addEventListener("dragover", function(event) {
-        // Prevent the default behavior (prevent the card from being dropped outside of the dropzone)
-        event.preventDefault();
-      });
+  });
+
+  // Add the drop event listener to the dropzones
+  dropzones.forEach(function(dropzone) {
+    dropzone.addEventListener("drop", function(event) {
+      // Prevent the default behavior (prevent the card from being dropped outside of the dropzone)
+      event.preventDefault();
+
+      // Get the data type and the value of the drag data
+      var data = event.dataTransfer.getData("text/plain");
+
+      // Append the card to the dropzone
+      event.target.appendChild(document.getElementById(data));
     });
-  
-    // Add the drop event listener to the dropzones
-    dropzones.forEach(function(dropzone) {
-      dropzone.addEventListener("drop", function(event) {
-        // Prevent the default behavior (prevent the card from being dropped outside of the dropzone)
-        event.preventDefault();
-  
-        // Get the data type and the value of the drag data
-        var data = event.dataTransfer.getData("text/plain");
-  
-        // Append the card to the dropzone
-        event.target.appendChild(document.getElementById(data));
-      });
-    });
+  });
+
+  // Add the contextmenu event listener to the card
+  card.addEventListener("contextmenu", function(event) {
+    // Prevent the default behavior (prevent the context menu from being displayed)
+    event.preventDefault();
+
+    // Append the card to the original div
+    originalDiv.appendChild(card);
+  });
 
   
   
